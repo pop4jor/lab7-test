37cf18b7a39d2287239998a1fa522ed1712ba48a
pop4jor <mailforskfu@gmail.com>
2026-06-14 18:43:49 +0300

    Update ауфафу.kt

--- ауфафу.kt
+++ ауфафу.kt
@@ -1,1 +1,15 @@
 
+post("/api/login") {
+            // Принимаем JSON и автоматически конвертируем его в дата-класс LoginRequest
+            val request = call.receive<LoginRequest>()
+            
+            // Бизнес-логика проверки пользователя через БД
+            val authResponse = DatabaseFactory.authenticateUser(request)
+            
+            // Отправляем ответ клиенту в формате JSON со статусом HTTP
+            if (authResponse.success) {
+                call.respond(HttpStatusCode.OK, authResponse)
+            } else {
+                call.respond(HttpStatusCode.Unauthorized, authResponse)
+            }
+        }
